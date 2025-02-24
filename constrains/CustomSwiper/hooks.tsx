042fa460c49c77642cabfc1f7b7928f1f70884a1
luke.zhu <luke.zhu@thinkchina.com>
2025-02-24 16:43:53 +1100

    clean code

---
+++ constrains/CustomSwiper/hooks.tsx
@@ -0,0 +1,126 @@
+import { useState, useEffect } from "react";
+
+const useDrags = ({
+  prevSlide,
+  nextSlide,
+}: {
+  prevSlide: () => void;
+  nextSlide: () => void;
+}) => {
+  const rule1 = 280;
+  const rule2 = 180;
+  const [translateXOffSet, setTranslateXOffset] = useState(0);
+  const [dragging, setDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener("mouseleave", handleDragEnd);
+    return () => {
+      document.removeEventListener("mouseleave", handleDragEnd);
+    };
+  }, []);
+  const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
+    const clientX = "touches" in e ? e.touches[0].clientX : e.clientX;
+    setDragging(true);
+    setStartX(clientX);
+  };
+
+  const handleDragMove = (e: React.MouseEvent | React.TouchEvent) => {
+    if (!dragging) return;
+    const clientX = "touches" in e ? e.touches[0].clientX : e.clientX;
+    setTranslateXOffset(clientX - startX);
+  };
+
+  const handleDragEnd = () => {
+    if (!dragging) return;
+    setDragging(false);
+    let offsetTimes = 0;
+    if (translateXOffSet < 0) {
+      offsetTimes = Math.floor(Math.abs(translateXOffSet) / rule1);
+    } else {
+      offsetTimes = Math.floor(Math.abs(translateXOffSet) / rule2);
+    }
+    if (offsetTimes >= 1) {
+      if (translateXOffSet > 0) {
+        prevSlide();
+      } else if (translateXOffSet <= 0) {
+        nextSlide();
+      }
+    }
+
+    setTranslateXOffset(0);
+  };
+  return { handleDragStart, handleDragMove, handleDragEnd, translateXOffSet };
+};
+
+const useMove = () => {
+  const images = [
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+    "/aqib-talib-image.png",
+  ];
+
+  const offsetLeft = 64;
+  const offsetRight = 40;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [translateX, setTranslateX] = useState(
+    images.map((_, index) => {
+      return index * offsetLeft;
+    })
+  );
+  const nextSlide = () => {
+    if (currentIndex < images.length - 1) {
+      const nIndex = currentIndex + 1;
+      const nX = translateX.map((_, index) => {
+        if (index < nIndex) {
+          // const offset = nIndex - index;
+          return _ - offsetRight;
+        } else {
+          // const offset = index - nIndex;
+          return _ - offsetLeft;
+        }
+      });
+      setTranslateX(nX);
+    }
+    setCurrentIndex((prevIndex) =>
+      prevIndex < images.length - 1 ? prevIndex + 1 : prevIndex
+    );
+  };
+
+  // 上一张
+  const prevSlide = () => {
+    if (currentIndex > 0) {
+      const nIndex = currentIndex - 1;
+      const nX = translateX.map((_, index) => {
+        if (index <= nIndex) {
+          // const offset = nIndex - index;
+          return _ + offsetRight;
+        } else {
+          // const offset = index - nIndex;
+          return _ + offsetLeft;
+        }
+      });
+      setTranslateX(nX);
+      setCurrentIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : prevIndex
+      );
+    }
+  };
+  return { nextSlide, prevSlide, translateX, currentIndex, images };
+};
+
+export { useDrags, useMove };
--- constrains/CustomSwiper/index.tsx
+++ constrains/CustomSwiper/index.tsx
@@ -1,79 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import Card from "@/components/PersonCard";
 import Bkg from "@/components/BkgTrapezoid";
 import Summary from "@/components/Summary";
+import { useDrags, useMove } from "./hooks";
 export default function CustomSwiper() {
-  const images = [
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-    "/aqib-talib-image.png",
-  ];
-
-  const offsetLeft = 64;
-  const offsetRight = 40;
-  const rule1 = 280;
-  const rule2 = 180;
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [translateX, setTranslateX] = useState(
-    images.map((_, index) => {
-      return index * offsetLeft;
-    })
-  );
-  const [translateXOffSet, setTranslateXOffset] = useState(0);
-  const [dragging, setDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-
-  useEffect(() => {
-    document.addEventListener("mouseleave", handleDragEnd);
-    return () => {
-      document.removeEventListener("mouseleave", handleDragEnd);
-    };
-  }, []);
-
-  const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
-    const clientX = "touches" in e ? e.touches[0].clientX : e.clientX;
-    setDragging(true);
-    setStartX(clientX);
-  };
-  const handleDragMove = (e: React.MouseEvent | React.TouchEvent) => {
-    if (!dragging) return;
-    const clientX = "touches" in e ? e.touches[0].clientX : e.clientX;
-    setTranslateXOffset(clientX - startX);
-  };
-  const handleDragEnd = () => {
-    setDragging(false);
-    let offsetTimes = 0;
-    if (translateXOffSet < 0) {
-      offsetTimes = Math.floor(Math.abs(translateXOffSet) / rule1);
-    } else {
-      offsetTimes = Math.floor(Math.abs(translateXOffSet) / rule2);
-    }
-    if (offsetTimes >= 1) {
-      if (translateXOffSet > 0) {
-        prevSlide();
-      } else if (translateXOffSet <= 0) {
-        nextSlide();
-      }
-    }
-
-    setTranslateXOffset(0);
-  };
-
   // const nextSlide1 = (times: number) => {
   //   const maxTimes = images.length - 1 - currentIndex;
   //   const t = times > maxTimes ? maxTimes : times;
@@ -110,52 +41,18 @@ export default function CustomSwiper() {
   //   setCurrentIndex(currentIndex - t);
   // };
 
+  const { nextSlide, prevSlide, translateX, currentIndex, images } = useMove();
+  const { handleDragStart, handleDragMove, handleDragEnd, translateXOffSet } =
+    useDrags({
+      prevSlide,
+      nextSlide,
+    });
   const calStyles = (index: number) => {
     const x = translateX[index];
     return {
       transform: `translateX(calc(${x}vw + ${translateXOffSet}px))`,
     };
   };
-
-  const nextSlide = () => {
-    if (currentIndex < images.length - 1) {
-      const nIndex = currentIndex + 1;
-      const nX = translateX.map((_, index) => {
-        if (index < nIndex) {
-          // const offset = nIndex - index;
-          return _ - offsetRight;
-        } else {
-          // const offset = index - nIndex;
-          return _ - offsetLeft;
-        }
-      });
-      setTranslateX(nX);
-    }
-    setCurrentIndex((prevIndex) =>
-      prevIndex < images.length - 1 ? prevIndex + 1 : prevIndex
-    );
-  };
-
-  // 上一张
-  const prevSlide = () => {
-    if (currentIndex > 0) {
-      const nIndex = currentIndex - 1;
-      const nX = translateX.map((_, index) => {
-        if (index <= nIndex) {
-          // const offset = nIndex - index;
-          return _ + offsetRight;
-        } else {
-          // const offset = index - nIndex;
-          return _ + offsetLeft;
-        }
-      });
-      setTranslateX(nX);
-      setCurrentIndex((prevIndex) =>
-        prevIndex > 0 ? prevIndex - 1 : prevIndex
-      );
-    }
-  };
-
   return (
     <div className={styles.swiper}>
       <Bkg></Bkg>
