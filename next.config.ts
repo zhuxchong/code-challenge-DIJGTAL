e812e745b2e532374cfe437da283cf893eafed5b
luke.zhu <luke.zhu@thinkchina.com>
2025-02-27 08:07:14 +1100

    webpack

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
-// import path from "path";
-const nextConfig: NextConfig = {};
+import path from "path";
+const nextConfig: NextConfig = {
+  webpack(config) {
+    //@ALIAS
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src/"),
+      lodash: path.resolve(__dirname, "node_modules", "lodash-es"),
+    };
+
+    //SVG LOADING
+    config.module.rules.push({
+      test: /\.svg/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
